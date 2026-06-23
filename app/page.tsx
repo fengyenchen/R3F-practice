31fa90d8a62ec8f9ce30d5d0026ca44776fa475d
fengyenchen <feng20050504@gmail.com>
2026-06-24 00:24:46 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex-col items-center justify-center bg-background text-foreground">
+      
+    </div>
+  );
+}
