5ad60c9da4993dcf4d5465596785ea37d2ef9479
fengyenchen <feng20050504@gmail.com>
2026-06-26 19:53:29 +0800

    wall

---
+++ app/wall/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Wall from "./Wall";
+
+export default function WallPage() {
+  return (
+    <main className="relative w-full h-screen overflow-hidden bg-[#0D0A10] flex items-center justify-center">
+
+      <Wall />
+
+    </main>
+  );
+}
